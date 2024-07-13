03c20b51641a35f17496e856824e20ef4ca06fb7
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-07-13 18:39:50 +0530

    pcc,hrd states

--- src/Components/HrdPage/KaranatakaHrd/KaranatakaHrdHead/KaranatakaHrdHead.jsx
+++ src/Components/HrdPage/KaranatakaHrd/KaranatakaHrdHead/KaranatakaHrdHead.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const KaranatakaHrdHead = () => {
   return (
-    <div className='karHrdHead'>
-      
+    <div className="kaHeadContainer">
+    <div className="kaHeadCover">
+      <h2>
+      HRD Attestation in Karnataka by <span>Goodway Attestation</span>
+      </h2>
     </div>
+    <img src="" alt="keralaHrd" />
+  </div>
   )
 }
 
--- src/Components/HrdPage/KeralaHrd/KeralaHead.jsx
+++ src/Components/HrdPage/KeralaHrd/KeralaHead.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import keralaHrd from "./../../../vid&img/Hrd/KeralaHrd.png";
 
 const KeralaHead = () => {
   return (
-    <div>
-      
+    <div className="mofaHeadContainer">
+      <div className="mofaHeadCover">
+        <h2>
+          MOFA <span>Attestation</span> Services
+        </h2>
+      </div>
+      <img src={keralaHrd} alt="keralaHrd" />
     </div>
-  )
-}
+  );
+};
 
-export default KeralaHead
+export default KeralaHead;
